7dd0cf40e6b9d5873e357a5e6ae14487f17fd60d
eno <eno@open-lab.org>
2014-08-12 17:59:46 +0200

    smtpd: adds connection tests

--- test/listen.roundup.sh
+++ test/listen.roundup.sh
@@ -14,6 +14,19 @@ it_listens_on_2525_by_default() {
   ! tools/wait_port 2525
 }
 
+it_connects_properly() {
+  start_ssmtpd \
+      --hostname ssmtpd.test.local \
+      --ssl-key $PWD/fixtures/ssmtpd.test.local.priv  \
+      --ssl-cert $PWD/fixtures/ssmtpd.test.local.pem 
+  tools/wait_port 2525 1
+
+  swaks --to user@example.com \
+    --server localhost \
+    --port 2525 \
+    --quit-after CONNECT
+}
+
 it_listens_on_custom_port() {
   start_ssmtpd \
       --hostname ssmtpd.test.local \
