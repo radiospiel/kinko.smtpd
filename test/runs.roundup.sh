ba3ac8efe27c1118b34549a4fc2857979de3cfa3
eno <eno@open-lab.org>
2014-08-12 15:23:49 +0200

    smtpd: adds first set of tests
    Also adds a wait_port script, which is used from the tests.

--- test/runs.roundup.sh
+++ test/runs.roundup.sh
@@ -3,16 +3,13 @@ describe "tests email en/decryption"
 
 . testhelper.inc
 
-#
-# If receiver's pubkeys are missing the encryption fails.
-#
-it_starts_and_stops() {
-  kinko.smtpd.configure 
-  kinko daemon:start smtpd
-  kinko daemon:wait smtpd
-  
-  wait_port 8025
+it_listens_on_2525_by_default() {
+  start_ssmtpd \
+      --hostname ssmtpd.test.local \
+      --ssl-key $PWD/fixtures/ssmtpd.test.local.priv  \
+      --ssl-cert $PWD/fixtures/ssmtpd.test.local.pem 
+  tools/wait_port 2525 1
 
-  kinko daemon:stop smtpd
-  ! wait_port 8025
+  stop_ssmtpd
+  ! tools/wait_port 2525
 }
